1b5f26bdc7c756a4dc821b4c51bb8fda2a1b4f46
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-03 16:12:11 -0300

    refactor src __init__.py
    Placing a comment just in case __init__ does not work for any of the members, will be required to follow instructions placed here.
    
    Direct push because we are not working as a team yet in the code

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,7 @@
 ##file serves as a marker for the classes/ directory as a Python package
+
+##If Init does not work for you, another alternative is placing code below at top of the file you're having issues at execution time
+
+##import os
+##import sys
+##sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
